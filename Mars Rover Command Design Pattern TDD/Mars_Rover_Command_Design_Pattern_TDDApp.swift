d8d8a69a639803e10967d77a00c47f85d1ef0da0
Resn4R <purview-joust.0x@icloud.com>
2023-11-30 18:42:41 +0000

    Initial Commit

---
+++ Mars Rover Command Design Pattern TDD/Mars_Rover_Command_Design_Pattern_TDDApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mars_Rover_Command_Design_Pattern_TDDApp.swift
+//  Mars Rover Command Design Pattern TDD
+//
+//  Created by Vito Borghi on 30/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct Mars_Rover_Command_Design_Pattern_TDDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
